63d38f64603f6db59c4b85df602555d554ed2e5c
Juliana <juliana6k@mail.ru>
2024-01-04 03:17:18 +0300

    3

--- venv/db_manager.py
+++ venv/db_manager.py
@@ -1,17 +1,26 @@
 import psycopg2
 
 class DBManager():
-    def get_companies_and_vacancies_count(self):
+    def get_companies_and_vacancies_count(self, cur):
         pass
-
     def get_all_vacancies(self):
-        pass
+        cur.execute(f"""Select employer, vacancy_name, salary_min, vacancy_url 
+        from vacancies order by salary_min DESC""")
+        result = cur.fetchall()
+        return result
+
 
     def get_avg_salary(self):
-        pass
+        cur.execute(f"""Select AVG(salary_min) from vacancies""")
+        result = cur.fetchall()
+        return result
 
     def get_vacancies_with_higher_salary(self):
-        pass
+        cur.execute(f"""Select employer, vacancy_name, salary_min, vacancy_url
+            from vacancies where salary_min > AVG(salary_min)
+            order by salary_min DESC""")
+        result = cur.fetchall()
+        return result
 
     def get_vacancies_with_keyword(self, keyword, cur):
         cur.execute(f"""Select * from vacancies where vacancy_name like %{keyword}%""")
--- venv/hh_api.py
+++ venv/hh_api.py
@@ -2,35 +2,49 @@ import requests
 import json
 
 
+employer_ids =[
+    "3127",  # Мегафон
+    "3529",  # Сбер
+    "78638",  # Тинькофф
+    "1740",  # Яндекс
+    "2748",  # Ростелеком
+    "3776",  # МТС
+    "2180",  # Ozon
+    "1122462",  # Skyeng
+    "15478",  # VK
+    "84585"  # Авито
+]
 class HH_vacancy():
+    """Класс для работы с API платформы HeadHunter"""
+
     HH_URL = "https://api.hh.ru/vacancies"
     HH_COMPANY = "https://api.hh.ru/employers"
     HH_AREAS = "https://api.hh.ru/suggests/areas"
-    company_id = 'id_companies.json'
+    company_id = "id_companies.json"
 
-    def get_company_id(self):
-        company_id_list = []
-        with open(self.company_id, encoding="utf-8") as file:
-            company_json = json.load(file)
-            for company in company_json:
-                company_id_list.append(company['id'])
-        return company_id_list
-
-
-    def get_company_name(self):
-        company_name_list = []
-        with open(self.company_id, encoding="utf-8") as file:
-            company_json = json.load(file)
-            for company in company_json:
-                company_name_list.append(company['name'])
-            return company_name_list
+    # def get_company_id(self):
+    #     company_id_list = []
+    #     with open(self.company_id, encoding="utf-8") as file:
+    #         company_json = json.load(file)
+    #         for company in company_json:
+    #             company_id_list.append(company['id'])
+    #     return company_id_list
+    #
+    #
+    # def get_company_name(self):
+    #     company_name_list = []
+    #     with open(self.company_id, encoding="utf-8") as file:
+    #         company_json = json.load(file)
+    #         for company in company_json:
+    #             company_name_list.append(company['name'])
+    #     return company_name_list
 
 
     def get_vacancy(self):
+        """Получение данных о вакансиях работодателя"""
         vacancy_list = []
-        employee_list = self.get_company_name()
-        for num_id in range(len(employee_list)):
-            params = {"employer_id": employee_list[num_id],
+        for employer_id in employer_ids:
+            params = {"employer_id": employer_id,
                       "per_page": 100,
                       "only_with_salary": True,
                       }
