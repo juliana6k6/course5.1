156c38d73c7f67e18294a5b1a0d8590511927276
Juliana <juliana6k@mail.ru>
2024-01-06 11:57:32 +0300

    10

--- venv/config.py
+++ venv/config.py
@@ -1,15 +1,17 @@
 from configparser import ConfigParser
 
-def config(filename='database.ini', section='postgresql'):
+
+def config(filename="database.ini", section="postgresql"):
     # create a parser
-    parser=ConfigParser
+    parser = ConfigParser()
     # read config file
     parser.read(filename)
     db = {}
     if parser.has_section(section):
-        params=parser.items(section)
+        params = parser.items(section)
         for param in params:
             db[param[0]] = param[1]
     else:
-        raise Excetion("'Section {0} is not found in the {1} file.'.format(section, filename)")
+        raise Exception(
+            'Section {0} is not found in the {1} file.'.format(section, filename))
     return db
